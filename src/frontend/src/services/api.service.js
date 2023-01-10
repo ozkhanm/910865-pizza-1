aaf3361fa4d3f628c893c980c6756ea2c099777a
Ozkhan Mamedov <ozhanm96@yandex.ru>
2023-01-10 07:10:36 +0300

    added api services: ReadOnly, Crud, Auth

---
+++ src/frontend/src/services/api.service.js
@@ -0,0 +1,68 @@
+import JwtService from "@/services/jwt.service";
+
+import axios from "@/plugins/axios";
+
+export class ReadOnlyApiService {
+  #resource;
+  constructor(resource) {
+    this.#resource = resource;
+  }
+
+  async query() {
+    const { data } = await axios.get(this.#resource);
+
+    return data;
+  }
+}
+
+export class CrudApiService extends ReadOnlyApiService {
+  #resource;
+  constructor(resource) {
+    super(resource);
+    this.#resource = resource;
+  }
+
+  async post(entity) {
+    const { data } = await axios.post(this.#resource, entity);
+
+    return data;
+  }
+
+  async put(entity) {
+    const { data } = await axios.put(this.#resource, entity);
+
+    return data;
+  }
+
+  async delete(id) {
+    const { data } = await axios.delete(`${this.#resource}/${id}`);
+
+    return data;
+  }
+}
+
+export class AuthApiService {
+  setAuthHeader() {
+    const token = JwtService.getToken();
+
+    axios.defaults.headers.common["Authorization"] = token ? `Bearer ${token}` : "";
+  }
+
+  async login(params) {
+    const { data } = await axios.post("login", params);
+
+    return data;
+  }
+
+  async logout() {
+    const { data } = await axios.delete("logout");
+
+    return data;
+  }
+
+  async getMe() {
+    const { data } = await axios.get("whoAmI");
+
+    return data;
+  }
+}
