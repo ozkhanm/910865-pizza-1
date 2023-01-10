a05ca064d30b66a3c587ab6eb99a4596b67b2128
Ozkhan Mamedov <ozhanm96@yandex.ru>
2023-01-10 05:38:26 +0300

    added axios plugin

---
+++ src/frontend/src/plugins/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "/api/",
+});
+
+axiosInstance.interceptors.response.use(res => res, e => Promise.reject(e));
+
+export default axiosInstance;
